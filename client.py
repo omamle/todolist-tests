a904c35bccfaf94539fc6683a5320478897b0546
Omer Levy <omerlevy25@gmail.com>
2020-07-19 23:04:02 +0300

    All server requests implemented (delete, in/complete)

--- client.py
+++ client.py
@@ -13,9 +13,10 @@ def get_task(task_id):
     return r.json()
 
 
+# create task doesn't use task_id in server.py implementation?
 def create_task(task_id, task_name, completed):
     params = {"task": task_name, "completed": completed}
-    r = requests.post(SERVER_URL.format(PATH=f"tasks/{task_id}"), json=params)
+    r = requests.post(SERVER_URL.format(PATH="tasks"), json=params)
     return r.json()
 
 
@@ -25,22 +26,27 @@ def modify_task(task_id, task_name, completed):
     return r.json()
 
 
-def task_completed():
-    pass
+def task_completed(task_id):
+    r = requests.post(SERVER_URL.format(PATH=f"tasks/{task_id}/completed"))
+    return r.json()
 
 
-def task_incomplete():
-    pass
+def task_incomplete(task_id):
+    r = requests.post(SERVER_URL.format(PATH=f"tasks/{task_id}/incomplete"))
+    return r.json()
 
 
 def delete_task():
-    pass
+    r = requests.delete(SERVER_URL.format(PATH=f"tasks/{task_id}"))
+    return r.json()
 
 
 def main():
     try:
         print(list_tasks())
         print(get_task("zJX8KXIe"))
+        print(create_task(123, "task_123", False))
+        print(list_tasks())
     except requests.exceptions.ConnectionError:
         print("Failed to connect to server.")
 
