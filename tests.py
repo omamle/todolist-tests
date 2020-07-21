d9ddfd5341835fe413d0ccc3ab77a63e7db2f5a6
Omer Levy <omerlevy25@gmail.com>
2020-07-21 18:35:18 +0300

    added connectivity check for tests

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
+import requests
 import client
 
 
 def test_list_tasks():
+    tasks = client.list_tasks()
+
     return True
 
 
 def test_get_task():
-    pass
+    tasks = client.list_tasks()
+
+    return True
 
 
 def test_create_tasks():
@@ -40,12 +45,17 @@ def run_tests():
 
     success_count = 0
 
-    for test in tests:
-        if test():
-            print "TEST {} PASSED".format(test.func_name)
-            success_count += 1
-        else:
-            print "TEST {} FAILED".format(test.func_name)
+    try:
+        for test in tests:
+            if test():
+                print "TEST {} PASSED".format(test.func_name)
+                success_count += 1
+            else:
+                print "TEST {} FAILED".format(test.func_name)
+    except requests.exceptions.ConnectionError:
+        print "Failed to connect to server."
+        print "Make sure it's running on {}".format(client.SERVER_URL)
+        print "or modify SERVER_URL in client.py accordingly"
 
     print "PASSED {}/{} TESTS".format(success_count, len(tests))
 
