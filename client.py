6c3346700b61e8300927f4dea20e5086d9773d1b
Omer Levy <omerlevy25@gmail.com>
2020-07-22 20:27:31 +0300

    added request function as wrapper to server requests

--- client.py
+++ client.py
@@ -3,64 +3,65 @@ import requests
 SERVER_URL = "http://localhost:5000/{PATH}"
 
 
-def list_tasks():
-    r = requests.get(SERVER_URL.format(PATH="tasks"))
+def request(request_info):
+    """
+    Wrapper to send a request to the server, according to request_info.
+    :param request_info: a tuple in the following format -
+                        (request_method, url, params)
+                        each detailed as inner_param below.
+    :inner_param request_method: a http request type from requests.
+                        for example: requests.get
+    :inner_param url: the URL to send request to
+    :inner_param params: additional parameters to the request.
+                        (if none - {} or None)
+    :returns: the json response from the server
+    :raises AssertionError: if request_info  length isn't 3
+    :raises ConnectionError: when can't connect to server TODO:
+    :raises AssertionError: if response status code isn't 200
+    """
+    assert len(request_info) == 3, "Missing parameters"
+    request_method, url, params = request_info
+    try:
+        r = request_method(url, json=params)
+    except requests.exceptions.ConnectionError:
+        raise requests.exceptions.ConnectionError, \
+              "Failed to connect to server"
 
-    assert r.status_code == 200, \
-           "Error {} in list_tasks".format(r.status_code)
+    assert r.status_code == 200, "Error {}".format(r.status_code)
+
     return r.json()
+
+
+def list_tasks():
+    return (requests.get, SERVER_URL.format(PATH="tasks"), None)
 
 
 def get_task(task_id):
     path = "tasks/{ID}".format(ID=task_id)
-    r = requests.get(SERVER_URL.format(PATH=path))
-
-    assert r.status_code == 200, \
-           "Error {} in get_task".format(r.status_code)
-    return r.json()
+    return (requests.get, SERVER_URL.format(PATH=path), None)
 
 
 def create_task(task_name, completed):
     params = {"task": task_name, "completed": completed}
-    r = requests.post(SERVER_URL.format(PATH="tasks"), json=params)
-
-    assert r.status_code == 200, \
-           "Error {} in create_task".format(r.status_code)
-    return r.json()
+    return (requests.post, SERVER_URL.format(PATH="tasks"), params)
 
 
 def modify_task(task_id, task_name, completed):
     params = {"task": task_name, "completed": completed}
     path = "tasks/{ID}".format(ID=task_id)
-    r = requests.put(SERVER_URL.format(PATH=path), json=params)
-
-    assert r.status_code == 200, \
-           "Error {} in modify_task".format(r.status_code)
-    return r.json()
+    return (requests.put, SERVER_URL.format(PATH=path), params)
 
 
 def task_completed(task_id):
     path = "tasks/{ID}/completed".format(ID=task_id)
-    r = requests.post(SERVER_URL.format(PATH=path))
-
-    assert r.status_code == 200, \
-           "Error {} in task_completed".format(r.status_code)
-    return r.json()
+    return (requests.post, SERVER_URL.format(PATH=path), None)
 
 
 def task_incomplete(task_id):
     path = "tasks/{ID}/incomplete".format(ID=task_id)
-    r = requests.post(SERVER_URL.format(PATH=path))
-
-    assert r.status_code == 200, \
-           "Error {} in task_incomplete".format(r.status_code)
-    return r.json()
+    return (requests.post, SERVER_URL.format(PATH=path), None)
 
 
 def delete_task(task_id):
     path = "tasks/{ID}".format(ID=task_id)
-    r = requests.delete(SERVER_URL.format(PATH=path))
-
-    assert r.status_code == 200, \
-           "Error {} in delete_task".format(r.status_code)
-    return r.json()
+    return (requests.delete, SERVER_URL.format(PATH=path), None)
--- test_todolist.py
+++ test_todolist.py
@@ -1,19 +1,20 @@
 import requests
-import client
+from client import request, list_tasks, get_task, create_task, modify_task, \
+    task_completed, task_incomplete, delete_task
 import pytest
 
 
 @pytest.mark.dependency()
 def test_list_tasks():
-    tasks = client.list_tasks()
+    tasks = request(list_tasks())
     assert type(tasks) == list
 
 
 @pytest.mark.dependency(depends=["test_list_tasks"])
 def test_create_task():
-    old_tasks = client.list_tasks()
-    created_task = client.create_task("created", False)
-    new_tasks = client.list_tasks()
+    old_tasks = request(list_tasks())
+    created_task = request(create_task("created", False))
+    new_tasks = request(list_tasks())
     new_id = created_task["task_id"]
 
     # make sure task was created and wasn't there before
@@ -23,30 +24,30 @@ def test_create_task():
 
 @pytest.mark.dependency(depends=["test_list_tasks"])
 def test_get_task():
-    tasks = client.list_tasks()
+    tasks = request(list_tasks())
     assert len(tasks) > 0, "No tasks to get"
 
     existing_id = tasks[0]["id"]
-    task = client.get_task(existing_id)
+    task = request(get_task(existing_id))
 
     assert tasks[0]["task"] == task["task"]
     assert tasks[0]["completed"] == task["completed"]
 
     # TODO:
     # try to get non-existing ID
-    # task = client.get_task("Non_Existent")
+    # task = request(get_task("Non_Existent"))
     # print task
 
 
 @pytest.mark.dependency(depends=["test_list_tasks", "test_get_task"])
 def test_modify_task():
-    tasks = client.list_tasks()
+    tasks = request(list_tasks())
     assert len(tasks) > 0, "No tasks to modify"
 
     # modify first task in list
     old_task = tasks[0]
-    client.modify_task(old_task["id"], "modified_task", True)
-    modified_task = client.get_task(old_task["id"])
+    request(modify_task(old_task["id"], "modified_task", True))
+    modified_task = request(get_task(old_task["id"]))
 
     assert modified_task["task"] == "modified_task"
     assert modified_task["completed"]
@@ -54,13 +55,13 @@ def test_modify_task():
 
 @pytest.mark.dependency(depends=["test_create_task", "test_get_task"])
 def test_task_complete():
-    task_id = client.create_task("incomplete_task", False)["task_id"]
-    client.task_completed(task_id)
-    complete = client.get_task(task_id)
+    task_id = request(create_task("incomplete_task", False))["task_id"]
+    request(task_completed(task_id))
+    complete = request(get_task(task_id))
 
     # make sure marking complete again doesn't affect it
-    client.task_completed(task_id)
-    still_complete = client.get_task(task_id)
+    request(task_completed(task_id))
+    still_complete = request(get_task(task_id))
 
     assert complete["completed"]
     assert still_complete["completed"]
@@ -68,13 +69,13 @@ def test_task_complete():
 
 @pytest.mark.dependency(depends=["test_create_task", "test_get_task"])
 def test_task_incomplete():
-    task_id = client.create_task("complete_task", True)["task_id"]
-    client.task_incomplete(task_id)
-    incomplete = client.get_task(task_id)
+    task_id = request(create_task("complete_task", True))["task_id"]
+    request(task_incomplete(task_id))
+    incomplete = request(get_task(task_id))
 
     # make sure marking incomplete again doesn't affect it
-    client.task_incomplete(task_id)
-    still_incomplete = client.get_task(task_id)
+    request(task_incomplete(task_id))
+    still_incomplete = request(get_task(task_id))
 
     assert not incomplete["completed"] 
     assert not still_incomplete["completed"]
@@ -82,10 +83,10 @@ def test_task_incomplete():
 
 @pytest.mark.dependency(depends=["test_list_tasks", "test_create_task"])
 def test_delete_tasks():
-    task_id = client.create_task("to_delete", False)["task_id"]
-    list_before = client.list_tasks()
-    client.delete_task(task_id)
-    list_after = client.list_tasks()
+    task_id = request(create_task("to_delete", False))["task_id"]
+    list_before = request(list_tasks())
+    request(delete_task(task_id))
+    list_after = request(list_tasks())
 
     # make sure task was in list before and isn't in list after
     assert any([task_id == task["id"] for task in list_before])
